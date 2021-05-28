39520f015f9fa877695e7f79a80c235add7c5737
Eric Hanley <80992779+hanleye29@users.noreply.github.com>
2021-05-28 16:18:26 -0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-API_Key = 'EYeAWPuDaqPX52Q70oYS9F31l'
-API_Secret_Key = 'ntt1T8r8IH6fsPvo3AGhhTsca5uFXwstbKVPdvYju3ELvbD38s'
-Access_Token = '1397377898714177536-XBX72edUL0KrRvjXIRJ7tQEcRnf5ZG'
-Access_Token_Secret = 'UVHK4dYJIdTaWfqjeNmmp94twlHlbxDp1EHEAGpBhdt7n'
